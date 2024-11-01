51a0f6222c45187fd7b127147cc4c76e66522a00
Joabesv <joabevarjao123@gmail.com>
2024-11-01 17:45:52 -0300

    chore: add tests

---
+++ test/app/not-found-handler.test.ts
@@ -0,0 +1,35 @@
+import assert from 'node:assert'
+import { it } from 'node:test'
+import { build } from '../helper.js'
+
+it('should call notFoundHandler', async (t) => {
+  const app = await build(t)
+
+  const res = await app.inject({
+    method: 'GET',
+    url: '/this-route-does-not-exist',
+  })
+
+  assert.strictEqual(res.statusCode, 404)
+  assert.deepStrictEqual(JSON.parse(res.payload), { message: 'Not Found' })
+})
+
+it('should be rate limited', async (t) => {
+  const app = await build(t)
+  app.log.warn(app.config)
+  for (let i = 0; i < 3; i++) {
+    const res = await app.inject({
+      method: 'GET',
+      url: '/this-route-does-not-exist',
+    })
+
+    assert.strictEqual(res.statusCode, 404, `Iteration ${i}`)
+  }
+
+  const res = await app.inject({
+    method: 'GET',
+    url: '/this-route-does-not-exist',
+  })
+
+  assert.strictEqual(res.statusCode, 429, 'Expected 429')
+})
